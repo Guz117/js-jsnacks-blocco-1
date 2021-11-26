1450fb2626fb116f9763d2801a2d5158a86f6636
Guz117 <cristian.guzman117@gmail.com>
2021-11-26 14:41:54 +0100

    somma dei numeri

---
+++ jsnack1/script.js
@@ -0,0 +1,12 @@
+/*
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    const number =  parseInt(prompt('inserisci un numero'));
+    sum += number;
+}
+console.log(sum)
+document.getElementById('somma').innerHTML = `La somma dei numeri inseriti è ${sum}`
