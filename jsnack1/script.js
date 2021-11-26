44777ee494bfca9bae6d3323097a10c5b2d3a260
Guz117 <cristian.guzman117@gmail.com>
2021-11-26 14:48:43 +0100

    somma dei numeri ciclo while

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,10 +3,23 @@ Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 */
 
+// let sum = 0;
+// for (let i = 0; i < 10; i++) {
+//     const number =  parseInt(prompt('inserisci un numero'));
+//     sum += number;
+// }
+// console.log(sum)
+// document.getElementById('somma').innerHTML = `La somma dei numeri inseriti è ${sum}`;
+
+
 let sum = 0;
-for (let i = 0; i < 10; i++) {
+let i = 0;
+
+while(i < 10) {
     const number =  parseInt(prompt('inserisci un numero'));
     sum += number;
+    i++;
 }
+
 console.log(sum)
-document.getElementById('somma').innerHTML = `La somma dei numeri inseriti è ${sum}`
+document.getElementById('somma').innerHTML = `La somma dei numeri inseriti è ${sum}`;
