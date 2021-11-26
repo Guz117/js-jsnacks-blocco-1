c75e5a82036d3a8da2f03b6ff9c18e381a68fab5
Guz117 <cristian.guzman117@gmail.com>
2021-11-26 15:04:36 +0100

    lista invitati in console

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,3 +2,48 @@
 In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
 chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 */
+
+// Ciclo for
+const invitatiGatsby = [
+    'Leonardo',
+    'Tobey',
+    'Carey',
+    'Joel',
+    'Elizabeth'
+]
+
+const nomeInvitato = prompt('Inserisci il tuo nome');
+
+// let find = false;
+
+// for (let i = 0; i < invitatiGatsby.length; i++) {
+//     const element = invitatiGatsby[i];
+//     if (nomeInvitato.toLowerCase() == element.toLowerCase()) {
+//         find = true;
+//     } 
+// } 
+
+// console.log(find);
+// if (find) {
+//     console.log('Benvenuto alla festa');
+// } else {
+//     console.log('Mi dispiace non è nella lista degli invitati')
+// }
+
+
+// Ciclo while
+let i = 0;
+let find = false;
+while (i < invitatiGatsby.length) {
+    const element = invitatiGatsby[i];
+    if (nomeInvitato.toLowerCase() == element.toLowerCase()) {
+        find = true;
+    } 
+    i++;
+}
+
+if (find) {
+    console.log('Benvenuto alla festa');
+} else {
+    console.log('Mi dispiace non è nella lista degli invitati')
+}
